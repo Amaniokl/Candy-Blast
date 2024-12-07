31580de459584cd6e44df183ceed0e4caed53b24
Amaniokl <amanoutlook2003@gmail.com>
2024-12-07 22:11:02 +0530

    added candy pop functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,51 @@ import { useAppDispatch, useAppSelector } from "./store/hooks";
 import Board from "./components/Board";
 import Tile from "./components/Tile";
 import { updateBoard } from "./store";
-
+import { checkForRowOfFour, checkForRowOfThree, isColumnOfFour, isColumnOfThree } from "./utils/moveCheckLogic";
+import { formulaForColumnOfFour, formulaForColumnOfThree, generateInvalidMoves } from "./utils/formulas";
+// import isRowOfFour
 function App() {
   const dispatch = useAppDispatch();
-  const board = useAppSelector(({candyCrush: {board}}) => board);
+  const board = useAppSelector(({ candyCrush: { board } }) => board);
   const boardSize = useAppSelector(
-    ({candyCrush: {boardSize}}) => boardSize);
+    ({ candyCrush: { boardSize } }) => boardSize);
 
   useEffect(() => {
     dispatch(updateBoard(createBoard(boardSize)));
   }, [boardSize, dispatch]);
 
+  // useEffect(()=>{
+  //   dispatch(updateBoard(createBoard(boardSize)));
+  // }, [boardSize, dispatch]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const newBoard = [...board];
+      isColumnOfFour(
+        newBoard, 
+        boardSize, 
+        formulaForColumnOfFour(boardSize)
+      );
+      checkForRowOfFour(newBoard, 
+        boardSize, 
+        generateInvalidMoves(boardSize, true)
+      );
+      isColumnOfThree(newBoard, 
+        boardSize, 
+        formulaForColumnOfThree(boardSize)
+      );
+      checkForRowOfThree(newBoard, 
+        boardSize, 
+        generateInvalidMoves(boardSize)
+      );
+      dispatch(updateBoard(newBoard));
+
+    }, 150);
+    return () => clearInterval(timeout)
+  }, [board, boardSize, dispatch]);
+
   return <div className="flex items-center justify-center h-screen">
-    <Board/>
+    <Board />
   </div>
 }
 
---
+++ src/utils/moveCheckLogic.ts
@@ -0,0 +1,93 @@
+// import { formulaForColumnOfFour } from "./formulas";
+
+export const isColumnOfFour = (
+    newBoard: string[],
+    boardSize: number,
+    formulaForColumnOfFour: number
+) => {
+    for (let i: number = 0; i <= formulaForColumnOfFour; i++) {
+        const columnOfFour: number[] = [
+            i,
+            i + boardSize,
+            i + boardSize * 2,
+            i + boardSize * 3,
+        ];
+        const decidedColor: string = newBoard[i];
+
+        const isBlank: boolean = newBoard[i] === "";
+
+        if (
+            columnOfFour.every(
+                (square: number) => newBoard[square] === decidedColor && !isBlank
+            )
+        ) {
+            columnOfFour.forEach((square: number) => (newBoard[square] = ""));
+            return true;
+        }
+    }
+};
+
+export const isColumnOfThree = (
+    newBoard: string[],
+    boardSize: number,
+    formulaForColumnOfThree: number
+) => {
+    for (let i: number = 0; i <= formulaForColumnOfThree; i++) {
+        const columnOfThree: number[] = [
+            i,
+            i + boardSize,
+            i + boardSize * 2
+        ];
+        const decidedColor: string = newBoard[i];
+
+        const isBlank: boolean = newBoard[i] === "";
+
+        
+    }
+};
+
+export const checkForRowOfFour=(
+    newBoard:string[],
+    boardSize:number,
+    invalidMoves:number[]
+)=>{
+    for(let i:number=0; i<boardSize*boardSize; i++){
+        const rowOfFour=[i, i+1, i+2, i+3]
+        const decidedColor: string = newBoard[i];
+
+        const isBlank: boolean = newBoard[i] === "";
+        if(invalidMoves.includes(i))continue;
+
+        if (
+            rowOfFour.every(
+                (square: number) => newBoard[square] === decidedColor && !isBlank
+            )
+        ) {
+            rowOfFour.forEach((square: number) => (newBoard[square] = ""));
+            return true;
+        }
+    }
+}
+
+export const checkForRowOfThree=(
+    newBoard:string[],
+    boardSize:number,
+    invalidMoves:number[]
+)=>{
+    for(let i:number=0; i<boardSize*boardSize; i++){
+        const rowOfThree=[i, i+1, i+2]
+        const decidedColor: string = newBoard[i];
+
+        const isBlank: boolean = newBoard[i] === "";
+        if(invalidMoves.includes(i))continue;
+
+        if (
+            rowOfThree.every(
+                (square: number) => newBoard[square] === decidedColor && !isBlank
+            )
+        ) {
+            rowOfThree.forEach((square: number) => (newBoard[square] = ""));
+            return true;
+        }
+    }
+}
